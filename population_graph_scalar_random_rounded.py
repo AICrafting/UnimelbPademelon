61d28742c5f0088a9b9f68cc549daf67a5ad38da
AICookingRPG <AICookingRPG@gmail.com>
2024-07-17 18:58:57 +1000

    Added check_for_extinction function

--- population_graph_scalar_random_rounded.py
+++ population_graph_scalar_random_rounded.py
@@ -7,14 +7,22 @@ alph = 0.15
 bet = 0.003
 lam = 0.18
 omeg = 0.0016
-random_max = 0.5
+random_max = 0.4
+pademelon_extinct = False
+thylacine_extinct = False
 
-def model(p, t):
+
+def model(p, t, pademelon_extinct, thylacine_extinct):
     x,y = p
-    dxdt = alph * x - bet * x * y
-    dydt = - lam * y + omeg * x * y
+    if not pademelon_extinct:
+        dxdt = alph * x - bet * x * y
+    else:
+        dxdt = 0
+    if not thylacine_extinct:
+        dydt = - lam * y + omeg * x * y
+    else:
+        dydt = 0
     return [dxdt, dydt]
-
 
 def check_boundaries(population):
     if population > 1000:
@@ -24,6 +32,12 @@ def check_boundaries(population):
     else:
         return population
 
+
+def check_for_extinction(population):
+    if population <= 0:
+        return True
+    else:
+        return False
 
 x0 = 180
 y0 = 30
@@ -36,7 +50,7 @@ thylacine_list = []
 while iterations > 0:
     t = np.linspace(100 - iterations, 101 - iterations, 11)
 
-    population_values = odeint(model, [x0, y0], t)
+    population_values = odeint(model, [x0, y0], t, (pademelon_extinct, thylacine_extinct))
 
     pademelon_population = population_values[:, 0]
     thylacine_population = population_values[:, 1]
@@ -44,16 +58,24 @@ while iterations > 0:
     pademelon_list_temp = list(pademelon_population)
     for x in pademelon_list_temp:
         integer = check_boundaries(x)
-        pademelon_list.append(integer)
-        pademelon_list[pademelon_list.index(integer)] = round(integer, 0)
+        integer = round(integer, 0)
+        if not pademelon_extinct:
+            pademelon_extinct = check_for_extinction(integer)
+            pademelon_list.append(integer)
+        else:
+            pademelon_list.append(0)
     pademelon_list.pop()
     x0 = check_boundaries(check_boundaries(pademelon_list_temp[10]) * (1 - random_max + random.randint(0, 100) / 50 * random_max))
 
     thylacine_list_temp = list(thylacine_population)
     for y in thylacine_list_temp:
         integer = check_boundaries(y)
-        thylacine_list.append(integer)
-        thylacine_list[thylacine_list.index(integer)] = round(integer, 0)
+        integer = round(integer, 0)
+        if not thylacine_extinct:
+            thylacine_extinct = check_for_extinction(integer)
+            thylacine_list.append(integer)
+        else:
+            thylacine_list.append(0)
     thylacine_list.pop()
     y0 = check_boundaries(check_boundaries(thylacine_list_temp[10]) * (1 - random_max + random.randint(0, 100) / 50 * random_max))
 
