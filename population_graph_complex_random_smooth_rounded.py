29774d09605f996126457b9c8835fb2e69782eb1
AICookingRPG <AICookingRPG@gmail.com>
2024-07-17 18:14:10 +1000

    Changed error in randomness code for smooth graphs

--- population_graph_complex_random_smooth_rounded.py
+++ population_graph_complex_random_smooth_rounded.py
@@ -9,10 +9,10 @@ lam = 0.18
 omeg = 0.0016
 random_max = 1.0
 
-def model(p, t, scalarx, scalary):
+def model(p, t, scalarx1, scalarx2, scalary1, scalary2):
     x,y = p
-    dxdt = alph * x - bet * x * y * scalarx
-    dydt = - lam * y + omeg * x * y * scalary
+    dxdt = alph * x * scalarx1 - bet * x * y * scalarx2
+    dydt = - lam * y * scalary1 + omeg * x * y * scalary2
     return [dxdt, dydt]
 
 x0 = 180
@@ -26,7 +26,8 @@ thylacine_list = []
 while iterations > 0:
     t = np.linspace(100 - iterations, 101 - iterations, 11)
 
-    population_values = odeint(model, [x0, y0], t, ((1 - random_max + random.randint(0, 100) / 50 * random_max), (1 - random_max + random.randint(0, 100) / 50 * random_max)))
+    population_values = odeint(model, [x0, y0], t, ((1 - random_max + random.randint(0, 100) / 50 * random_max), (1 - random_max + random.randint(0, 100) / 50 * random_max), (1 - random_max + random.randint(0, 100) / 50 * random_max), (1 - random_max + random.randint(0, 100) / 50 * random_max)))
+
 
     pademelon_population = population_values[:, 0]
     thylacine_population = population_values[:, 1]
